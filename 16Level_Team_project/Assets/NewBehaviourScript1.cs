0e5dfa3f43249da958d3e0c0acd584cd8db39483
ChunBae20 <drchunbae@gmail.com>
2025-06-12 09:58:55 +0900

    add : utf8 인코딩 추가
    add : utf8 인코딩 추가

---
+++ 16Level_Team_project/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    void Start()
+    {
+        //반갑습니다
+    }
+
+    void Update()
+    {
+        
+    }
+}
