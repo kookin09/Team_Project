f9a19f80bd60466611492aa52419394580a97306
unity9Parkjaehyun <game_samson@naver.com>
2025-06-16 19:34:53 +0900

    시행착오
    시행착오

--- 16Level_Team_project/Assets/Scripts/JHP_ClickEvent/ClickEvent.cs
+++ 16Level_Team_project/Assets/Scripts/JHP_ClickEvent/ClickEvent.cs
@@ -59,6 +59,7 @@ public class ClickEvent : MonoBehaviour
     // 이 함수가 하는 일: 수동 클릭 공격을 실행합니다
     void OnMouseDown()
     {
+        Debug.Log("Enemy 클릭됨!");
         PerformAttack();
     }
 
@@ -131,7 +132,7 @@ public class ClickEvent : MonoBehaviour
     void PlayDebrisEffect(bool isCritical = false)
     {
         ParticleSystem targetParticle;
-
+        Debug.Log("파티클 재생 시도: " + transform.position);
         // 치명타 여부에 따라 사용할 파티클 선택
         if (isCritical && criticalParticle != null)
         {
